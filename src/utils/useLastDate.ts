d6436462d83d90d0c0609a5e9fdd31156fda2431
bintang.gabriel <bintang.gabriel@ui.ac.id>
2024-03-01 11:25:11 +0700

    fix date bug

--- src/utils/useLastDate.ts
+++ src/utils/useLastDate.ts
@@ -5,6 +5,10 @@ let lastDate = (Number(format(new Date(), "yyyyMMdd"))-1).toString();
 export const getLastDate = () => lastDate;
 
 export const setLastDate = (date:string) => {
-    // lastDate = (Number(date)- 1).toString();
-    lastDate = date;
+    if(date === "20240300"){
+        lastDate = "20240229"
+    }
+    else {
+        lastDate = date;
+    }
 }
